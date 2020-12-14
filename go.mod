b16b56e1afb87aa8265dcb0762796ca294f05ff5
Heri Gunawan <heri@tirto.id>
2020-12-14 22:06:04 +0700

    initial project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aageboi/go-crud
+
+go 1.13
+
+require github.com/aageboi/go-modules v1.1.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_mod "github.com/aageboi/go-modules"
+)
+
+func main() {
+	fmt.Println(go_mod.Hello("Aageboi"))
+}
